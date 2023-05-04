96b53674f2f945a4b580b84b7dc50a945d4da1a1
sangkyun <sangkyun@LAPTOP-U264MDMD>
2023-05-03 19:30:28 -0700

    (Test) image path

--- src/main/java/kr/co/mlec/Board/BoardController.java
+++ src/main/java/kr/co/mlec/Board/BoardController.java
@@ -128,7 +128,7 @@ public class BoardController {
 			// make image path
 			// need to change path (server)
 			// String path = "C:\\Users\\sangkyun\\Desktop\\new" + "ckImage/";
-			String path = "/opt/images";
+			String path = "/images/board";
 			String ckUploadPath = path + uid + "_" + fileName;
 			File folder = new File(path);
 
@@ -171,7 +171,7 @@ public class BoardController {
 	public void imageSubmit(@RequestParam(value = "uid") String uid, @RequestParam(value = "fileName") String fileName,
 			HttpServletRequest req, HttpServletResponse res) throws Exception {
 		// String path = "C:\\Users\\sangkyun\\Desktop\\new" + "ckImage/";
-		String path = "/opt/images";
+		String path = "/images/board";
 		String sDirPath = path + uid + "_" + fileName;
 		
 		File imgFile = new File(sDirPath);
